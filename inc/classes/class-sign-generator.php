1a3583784b9d73b0528e38e03e3b9a50f135a06c
shahjalal <rjshahjalal132@gmail.com>
2024-10-20 17:47:31 +0600

    created sign generate base route

--- inc/classes/class-sign-generator.php
+++ inc/classes/class-sign-generator.php
@@ -10,11 +10,33 @@ class Sign_Generator {
     use Singleton;
     use Program_Logs;
 
+    private $base_url;
+    private $partner_id;
+    private $partner_key;
+    private $shop_id;
+    private $access_token;
+
     public function __construct() {
         $this->setup_hooks();
     }
 
     public function setup_hooks() {
-        // setup hooks
+        add_action( 'rest_api_init', [ $this, 'register_api_endpoints' ] );
+    }
+
+    public function register_api_endpoints() {
+        register_rest_route( 'bulk-import/v1', '/sign-generate', [
+            'methods'             => 'GET',
+            'callback'            => [ $this, 'sign' ],
+            'permission_callback' => '__return_true',
+        ] );
+    }
+
+    public function sign( $request ) {
+        return $this->generate_sign();
+    }
+
+    public function generate_sign() {
+        return 'sign generated';
     }
 }
